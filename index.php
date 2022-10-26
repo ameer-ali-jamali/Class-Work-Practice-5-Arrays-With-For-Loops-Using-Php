ca5e69789fce9e93e2a4e2aa82e83ee0a29d956b
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-10-27 01:55:20 +0500

    array practice

--- index.php
+++ index.php
@@ -15,6 +15,17 @@
 
     <?php
 
+    $name = array(1 => "ameer", 2 => "amjad", 3 => "wasseem", 4 => "karem");
+    $fees = array("ameer" => 5500, "hakem" => 4300, "karem" => 5500, "junaid" => 5500);
+    $val = count($fees);
+    $var_dump($name);
+    echo $val;
+
+    // for($i=0,$i<=4,$i++){
+    //     echo "count $fees[$i] <br>";
+    // };
+
+
 
 
 
